6dcc641bb01341b4c543b4affdca1f1a11ab2e4c
Bree Hall <briannajdhall@gmail.com>
2022-04-03 13:11:02 -0400

    Implemented the ability to switch between HEX and RGB color codes for the game. Fixed a bug with the game timer

--- src/js/redesigned-game.js
+++ src/js/redesigned-game.js
@@ -10,13 +10,17 @@ const scoreLabel = document.getElementById('score');
 const endGame = document.getElementById('end-game');
 
 var gamePieces = [];
-var winningRGBVal;
-var gameDifficulty;
+var winningColorVal;
+var gameDifficulty = 'easy';
+var gameColorFormat = 'rgb';
 var gameInAction = false;
 var score = 0;
+var timeRemaining = 60;
+var timeHandlers;
 
 const winningText = '🎉 Congratulations! You won!';
 const losingText = '☹️ You may need some more practice';
+const playAgainButton = '<button id="play-again-btn" onClick="resetGame()">Play Again</button>'
 
 const createRGBVal = () => {
     const r = Math.floor(Math.random() * 256);
@@ -27,32 +31,39 @@ const createRGBVal = () => {
     return rgbVal;
 }
 
-const setGame = (difficulty) => {
+const creatHexVal = () => {
+    const hexVal = `#${(Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')}`;
+    return hexVal;
+}
+
+const setGame = (difficulty, colorFormat) => {
     gameDifficulty = difficulty;
+    gameColorFormat = colorFormat;
+
     if (difficulty === 'easy'){
         let winningIndex = Math.floor(Math.random() * 6);
 
         for (let i = 0; i < 6; i++){
-            let val = createRGBVal();
+            let val = gameColorFormat == 'rgb' ? createRGBVal() : creatHexVal();
             gamePieces[i] = val;
             if (i == winningIndex) {
-                winningRGBVal = val;
-                currentlyGuessing.textContent = winningRGBVal;
+                winningColorVal = val;
+                currentlyGuessing.textContent = winningColorVal;
             }
              
         }
     } else {
         let winningIndex = Math.floor(Math.random() * 9);
 
-        winningRGBVal = winningIndex;
-        currentlyGuessing.textContent = winningRGBVal;
+        winningColorVal = winningIndex;
+        currentlyGuessing.textContent = winningColorVal;
 
         for (let i = 0; i < 9; i++){
-            let val = createRGBVal();
+            let val = gameColorFormat == 'rgb' ? createRGBVal() : creatHexVal();
             gamePieces[i] = val;
             if (i == winningIndex) {
-                winningRGBVal = val;
-                currentlyGuessing.textContent = winningRGBVal;
+                winningColorVal = val;
+                currentlyGuessing.textContent = winningColorVal;
             }
         }
     }
@@ -65,13 +76,18 @@ const setGame = (difficulty) => {
 
 const guessColor = (event) => {
     const gamePieceSelected = event.target;
-    const colorGuessed = event.target.style.backgroundColor;
-
-    if (colorGuessed === winningRGBVal){
+    let colorGuessed;
+    if (gameColorFormat == 'hex'){
+        colorGuessed = rgb2hex(event.target.style.backgroundColor);
+    } else {
+        colorGuessed = event.target.style.backgroundColor;
+    }
+    
+    if (colorGuessed === winningColorVal){
         let allGamePieces = playArea.children;
 
         for (let i = 0; i < allGamePieces.length ; i++){
-            allGamePieces[i].style.backgroundColor = winningRGBVal;
+            allGamePieces[i].style.backgroundColor = winningColorVal;
             allGamePieces[i].classList.remove('incorrect-guess');
 
             const revealColorCode = allGamePieces[i].children[0];
@@ -93,65 +109,96 @@ const guessColor = (event) => {
 const resetBoard = () => {
     playArea.innerHTML = '';
     gamePieces = [];
-    winningRGBVal = '';
-    setGame(gameDifficulty);
+    winningColorVal = '';
+    setGame(gameDifficulty, gameColorFormat);
 }
 
 const resetGame = () => {
-    resetBoard();
+    //resetBoard();
+    playArea.innerHTML = '';
+    gamePieces = [];
+    winningColorVal = '';
     gameInAction = false;
     score = 0;
+    scoreLabel.textContent = '0';
+    // timeHandlers.stop();
+    timeRemaining = 60;
+    startGame();
 }
-
-const changeDifficulty = (event) => {
-    let newDifficulty = event.target.value;
-
-    if (newDifficulty === gameDifficulty){
-        return;
-    } else {
-        easyButton.classList.toggle('active');
-        hardButton.classList.toggle('active');
-        gameDifficulty = newDifficulty;
-        resetBoard();
-    }
-}
-
 
 const startGame = () => {
-    setGame('easy');
+    setGame(gameDifficulty, gameColorFormat);
     startGameButton.classList.add('hide');
     colorPrompt.classList.remove('hide');
     colorPrompt.classList.add('show-prompt-timer');
     //colorPrompt.classList.add('show');
     playArea.classList.remove('hide');
-    currentlyGuessing.textContent = winningRGBVal;
-    gameInAction = true;
-    setTimer();
+    mainGameArea.classList.remove('hide');
+    endGame.innerHTML = '';
+    currentlyGuessing.textContent = winningColorVal;
+    timeHandlers = setTimer();
 }
 
 const setTimer = () => {
-    let timeRemaining = 60;
+    // gameInAction = true;
     const countDown = setInterval(() => {
-      if(timeRemaining <= 0){
+
+        if(timeRemaining <= 0){
         clearInterval(countDown);
         gameInAction = false;
         finishGame();
-      }
+        }
 
-      if (timeRemaining == 60){
-          timer.textContent = '01:00';
-      } else if (timeRemaining > 9 ) {
+        if (timeRemaining == 60){
+            timer.textContent = '01:00';
+        } else if (timeRemaining > 9 ) {
         timer.textContent = `00:${timeRemaining}`;
-      } else {
+        } else {
         timer.textContent = `00:0${timeRemaining}`;
-      }
-      
-      timeRemaining -= 1;
+        }
+        
+        timeRemaining -= 1;
     }, 1000);
+
+    return {
+        stop () {
+            clearInterval(countDown);
+        }
+    }
 }
+
+
+
+
 
 const finishGame = () => {
     mainGameArea.classList.add('hide');
-
     score >= 5 ? endGame.textContent = winningText : endGame.textContent = losingText;
+    endGame.innerHTML += `<br><strong>Final Score:</strong> ${score}<br>`;
+    endGame.innerHTML += playAgainButton;
 }
+
+const rgb2hex = (rgb) => `#${rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/).slice(1).map(n => parseInt(n, 10).toString(16).padStart(2, '0')).join('')}`
+
+
+// CRUD Operations
+const setGameDifficulty = (newDifficulty) => {
+    gameDifficulty = newDifficulty;
+    timeHandlers.stop();
+    resetGame();
+    // timeRemaining = 60;
+    // countDown();
+}
+
+const setGameColorFormat = (newColorFormat) => {
+    gameColorFormat = newColorFormat;
+    timeHandlers.stop();
+    resetGame();
+    // timeRemaining = 60;
+    // countDown();
+
+}
+
+// window.onload = () => {
+//     startGame()
+// }
